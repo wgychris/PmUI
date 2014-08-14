00bddf4a45f59927a92471ba97dbe64ddb70ef6f
Yan <xiaoyanc210@gmail.com>
2014-08-14 17:15:50 -0400

    update filter in customer behaviour percentage

--- PieceMaker/WebContent/html/getWeeklyBehaviour.php
+++ PieceMaker/WebContent/html/getWeeklyBehaviour.php
@@ -10,6 +10,14 @@ $db= new SQLite3("./hello");
 
 $sql_select='SELECT DISTINCT strftime(\'%W-%Y\', Date) as week FROM DailyStatistics';
 
+$startDate = $_GET["startDate"];
+$endDate = $_GET["endDate"];
+
+if($startDate != null && $endDate != null) {
+	$sql_select = $sql_select.' WHERE Date >= \''.$startDate.'\' AND Date <=\''.$endDate.'\'';
+}
+
+
 $result=$db->query($sql_select);
 
 while($row = $result->fetchArray()) {
@@ -18,6 +26,10 @@ while($row = $result->fetchArray()) {
 	$selectRevenue = 'SELECT sum(NumOfOrder) as NumOfOrder, sum(NumOfCustomization) as NumOfCustomization, 
 			sum(NumOfFinalize) as NumOfFinalize, sum(NumOfView) as NumOfView FROM DailyStatistics WHERE strftime(\'%W-%Y\', Date) = \''.$weekStr.'\'';
 
+	if($startDate != null && $endDate != null) {
+		$selectRevenue = $selectRevenue.' AND Date >= \''.$startDate.'\' AND Date <=\''.$endDate.'\'';
+	}
+	
 	$tempResult = $db->query($selectRevenue);
 	$tempRow = $tempResult->fetchArray();
 
